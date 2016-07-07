5e18fec0281b1ae28bcd0a0fb015e261a5d85d0e
hezliz <hezliz@qq.com>
2016-07-07 17:33:44 +0800

    优化策略模式

---
+++ tools/tools-pattern/src/main/java/behaviour/strategy/Pay4ICBC.java
@@ -0,0 +1,24 @@
+/**
+ * tools-pattern
+ *
+ * Copyright(C)2016-2016 eduinline.com
+ *
+ * Created By eduinline Team on [2016年7月7日-下午5:12:33]
+ * 
+ */
+package behaviour.strategy;
+
+/**
+ * <p>ICBC支付实现类</p>
+ * 
+ * @version 1.0 2016年7月7日-下午5:12:33
+ * @author hongze.he@eduinline.com
+ */
+public class Pay4ICBC implements Pay {
+
+	public boolean doPay(String payParams) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
